d2662c851aef339681bd3d4b6837b36d3720ba2e
ng <nick.goupinets@gmail.com>
2019-07-12 18:28:53 -0400

    Finished lists

--- src/main/java/sandbox/lists/LinkedListNode.java
+++ src/main/java/sandbox/lists/LinkedListNode.java
@@ -15,23 +15,27 @@ public class LinkedListNode {
 	private int value;
 	private LinkedListNode next;
 
+	public LinkedListNode(int n) {
+		setValue(n);
+	}
+
 	public LinkedListNode removeDuplicatesInON() {
 		Set<Integer> visitedNodes = new HashSet<Integer>();
 		visitedNodes.add(this.getValue());
-		
+
 		LinkedListNode prev = this, next = this.next;
-		while(next != null) {
+		while (next != null) {
 			if (visitedNodes.contains(next.getValue())) {
 				prev.setNext(next.getNext());
 			} else {
 				visitedNodes.add(next.value);
 				prev = next;
-			}			
+			}
 			next = next.getNext();
 		}
 		return this;
 	}
-	
+
 	public LinkedListNode removeDuplicates() {
 		LinkedListNode node = this;
 		while (node != null) {
@@ -40,27 +44,119 @@ public class LinkedListNode {
 		}
 		return this;
 	}
-	
+
 	public LinkedListNode getKthToLast(int kth) {
 		int len = 0;
 		LinkedListNode n = this;
-		while(n != null) {
+		while (n != null) {
 			len++;
 			n = n.next;
 		}
-		
+
 		n = this;
-		for(int i = 1; i < (len - kth); i++) {
+		for (int i = 1; i < (len - kth); i++) {
 			n = n.next;
 		}
 		return n;
+	}
+
+	public LinkedListNode getKthToLastIterative(int kth) {
+		LinkedListNode ptr1 = this;
+		LinkedListNode ptr2 = this;
+
+		int oneBased = kth + 1;
+		for (int i = 0; i < oneBased; i++) {
+			if (ptr1 == null) {
+				return null;
+			}
+			ptr1 = ptr1.next;
+		}
+
+		while (ptr1 != null) {
+			ptr1 = ptr1.next;
+			ptr2 = ptr2.next;
+		}
+		return ptr2;
+	}
+
+	/**
+	 * CTCI6 - 2.5
+	 */
+	public LinkedListNode partition(int n) {
+		LinkedListNode lowerStart = null, lowerEnd = null;
+		LinkedListNode upperStart = null, upperEnd = null;
+
+		LinkedListNode next = this;
+		while (next != null) {
+			int val = next.getValue();
+			if (val >= n) {
+				if (upperStart == null) {
+					upperStart = new LinkedListNode(val);
+					upperEnd = upperStart;
+				} else {
+					LinkedListNode nn = new LinkedListNode(val);
+					upperEnd.next = nn;
+					upperEnd = nn;
+				}
+			} else {
+				if (lowerStart == null) {
+					lowerStart = new LinkedListNode(val);
+					lowerEnd = lowerStart;
+				} else {
+					LinkedListNode nn = new LinkedListNode(val);
+					lowerEnd.next = nn;
+					lowerEnd = nn;
+				}
+			}
+			next = next.getNext();
+		}
+		lowerEnd.next = upperStart;
+
+		return lowerStart;
+	}
+
+	/**
+	 * Deletes node from the list in O(1). Note that node must not be the last node
+	 * in the list, consider other options, like making it dummy for example.
+	 * 
+	 * @param n Node to remove.
+	 */
+	public void deleteNode(LinkedListNode n) {
+		if (n == null || n.next == null) {
+			return;
+		}
+
+		LinkedListNode next = n.next;
+		n.value = next.value;
+		n.next = next.next;
+	}
+
+	public LinkedListNode getKthToLastRecursive(int kth) {
+		return getKthToLastRecursive(kth + 1, this, new Index());
+	}
+
+	private LinkedListNode getKthToLastRecursive(int oneBasedIndex, LinkedListNode node, Index index) {
+		if (node == null) {
+			return null;
+		}
+
+		LinkedListNode n = getKthToLastRecursive(oneBasedIndex, node.next, index);
+		index.i++;
+		if (index.i == oneBasedIndex) {
+			return node;
+		}
+		return n;
+	}
+
+	private static final class Index {
+		int i = 0;
 	}
 
 	private void removeDups(LinkedListNode node, int value) {
 		if (node == null || node.next == null) {
 			return;
 		}
-		
+
 		if (node.next.value == value) {
 			node.next = node.next.next;
 		}
@@ -99,9 +195,108 @@ public class LinkedListNode {
 		result = prime * result + value;
 		return result;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "[" + value + "] -> " + next;
+	}
+
+	public int getLength() {
+		if (getNext() == null) {
+			return 1;
+		}
+		return 1 + getNext().getLength();
+	}
+
+	public LinkedListNode reverseAndClone() {
+		return reverseAndClone(this);
+	}
+
+	private LinkedListNode reverseAndClone(LinkedListNode node) {
+		LinkedListNode head = null;
+		while (node != null) {
+			LinkedListNode n = new LinkedListNode(node.getValue());
+			n.setNext(head);
+			head = n;
+			node = node.next;
+		}
+		return head;
+	}
+
+	public LinkedListNode isIntersecting(LinkedListNode node) {
+		if (node == null) {
+			return null;
+		}
+
+		LinkedListNode l1 = this;
+		LinkedListNode l2 = node;
+		int len1 = this.getLength();
+		int len2 = node.getLength();
+
+		LinkedListNode shorter = len1 < len2 ? l1 : l2;
+		LinkedListNode longer = len1 < len2 ? l2 : l1;
+
+		longer = longer.getKth(Math.abs(len1 - len2));
+		while (shorter != longer) {
+			shorter = shorter.next;
+			longer = longer.next;
+		}
+
+		return longer;
+	}
+
+	public LinkedListNode getKth(int k) {
+		LinkedListNode result = this;
+		while (k > 0 && result != null) {
+			result = result.next;
+			k--;
+		}
+		return result;
+	}
+
+	public LinkedListNode getLast() {
+		LinkedListNode result = this;
+		while (result.next != null) {
+			result = result.next;
+		}
+		return result;
+	}
+
+	public LinkedListNode getLoopStart() {
+		return getLoopStart(this);
+	}
+
+	private LinkedListNode getLoopStart(LinkedListNode head) {
+		LinkedListNode slow = head;
+		LinkedListNode fast = head;
+
+		// find meeting point - it will be LOOP_SIZE - k steps into the linked list
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			boolean isCollision = slow == fast;
+			if (isCollision) {
+				break;
+			}
+		}
+		
+		// if no meeting point (both cursors are null)
+		if (fast == null || fast.next == null) {
+			return null;
+		}
+		
+		// set slow to head, keep fast at the meeting point
+		// each are k steps from the loop start
+		slow = head;
+		
+		// move both at the same pace - they will meet at the loop start 
+		while(slow != fast) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+		
+		// both now point to the start of the loop
+		return fast;
 	}
 }
--- src/test/java/sandbox/lists/LinkedListNodeTest.java
+++ src/test/java/sandbox/lists/LinkedListNodeTest.java
@@ -6,22 +6,120 @@ import org.junit.jupiter.api.Test;
 
 class LinkedListNodeTest {
 
+	static LinkedListNode toList(int... nums) {
+		LinkedListNode head = null, node = null;
+		for (int i = 0; i < nums.length; i++) {
+			LinkedListNode n = new LinkedListNode();
+			n.setValue(nums[i]);
+			if (i == 0) {
+				head = n;
+			} else {
+				node.setNext(n);
+			}
+			node = n;
+
+		}
+		return head;
+	}
+
 	@Test
 	void shouldRemoveDuplicates() {
-		LinkedListNode list = new LinkedListNode(1, new LinkedListNode(2, new LinkedListNode(1, new LinkedListNode(2, null))));
-		LinkedListNode check = new LinkedListNode(1, new LinkedListNode(2, null));
+		LinkedListNode list = toList(1, 2, 1, 2);
+		LinkedListNode check = toList(1, 2);
 		assertEquals(check, list.removeDuplicates());
-		
-		list = new LinkedListNode(1, new LinkedListNode(2, new LinkedListNode(1, new LinkedListNode(2, null))));
-		check = new LinkedListNode(1, new LinkedListNode(2, null));
+
+		list = toList(1, 2, 1, 2);
+		check = toList(1, 2);
 		assertEquals(check, list.removeDuplicatesInON());
+	}
+
+	@Test
+	void shouldGetKthToLast() {
+		LinkedListNode list = toList(9, 2, 1, 10);
+		LinkedListNode kthToLast = list.getKthToLast(1);
+		assertEquals(kthToLast.getValue(), 1);
+
+		kthToLast = list.getKthToLastRecursive(0);
+		assertEquals(kthToLast.getValue(), 10);
+
+		kthToLast = list.getKthToLastIterative(0);
+		assertEquals(kthToLast.getValue(), 10);
+	}
+
+	@Test
+	void shouldPartition() {
+		LinkedListNode input = toList(3, 5, 8, 5, 10, 2, 1);
+		LinkedListNode partitionedOutput = input.partition(5);
+		for (int i = 0; i < 3; i++) {
+			assertTrue(partitionedOutput.getValue() < 5);
+			partitionedOutput = partitionedOutput.getNext();
+		}
+
+		for (int i = 3; i < 6; i++) {
+			assertTrue(partitionedOutput.getValue() >= 5);
+			partitionedOutput = partitionedOutput.getNext();
+		}
+	}
+
+	@Test
+	void shouldSum() {
+		LinkedListNode l1 = toList(7, 1, 6);
+		LinkedListNode l2 = toList(5, 9, 2);
+		assertEquals(toList(2, 1, 9), Utils.addReversedLists(l1, l2));
+	}
+
+	@Test
+	void shouldDetermineLength() {
+		LinkedListNode l1 = toList(1);
+		assertEquals(1, l1.getLength());
+		l1 = toList(1, 2, 3, 4, 5);
+		assertEquals(5, l1.getLength());
 	}
 	
 	@Test
-	void shouldGetKthToLast() {
-		LinkedListNode list = new LinkedListNode(9, new LinkedListNode(2, new LinkedListNode(1, new LinkedListNode(10, null))));
-		LinkedListNode kthToLast = list.getKthToLast(1);
-		assertEquals(kthToLast.getValue(), 1);
+	void shouldInverse() {
+		LinkedListNode l = toList(1, 2, 3, 4, 5);
+		assertEquals(toList(5, 4, 3, 2, 1), l.reverseAndClone());
 	}
-
+	
+	@Test
+	void shouldFindKthNode() {
+		LinkedListNode l = toList(1, 2, 3, 4, 5);
+		assertEquals(1, l.getKth(0).getValue());		
+		assertEquals(5, l.getKth(4).getValue());		
+	}
+	
+	@Test
+	void shouldFindNoIntersection() {
+		LinkedListNode l1 = toList(1, 2, 3, 4, 5);
+		LinkedListNode l2 = toList(1, 2, 3, 4, 5);
+		assertTrue(l1.isIntersecting(l2) == null);		
+	}
+	
+	@Test
+	void shouldFindIntersection() {
+		LinkedListNode tail = toList(98, 99, 100);
+		LinkedListNode l1 = toList(1, 2);
+		l1.getLast().setNext(tail);
+		LinkedListNode l2 = toList(1, 2, 3, 4, 5);
+		l2.setNext(tail);
+		
+		assertTrue(l1.isIntersecting(l2) == tail);		
+	}
+	
+	@Test
+	void shouldNotFindLoops() {
+		LinkedListNode l1 = toList(1, 2, 3);
+		assertFalse(l1.getLoopStart() != null);		
+	}
+	
+	@Test
+	void shouldFindLoops() {
+		LinkedListNode l1 = toList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		LinkedListNode kth = l1.getKth(4); // 5
+		LinkedListNode last = kth.getLast();
+		last.setNext(kth);
+		
+		assertTrue(l1.getLoopStart() == kth);		
+	}
 }
