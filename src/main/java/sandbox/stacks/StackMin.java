e9c1551fac1fe1076fde6b678a5d44a32aa04e93
ng <nick.goupinets@gmail.com>
2019-07-12 20:49:45 -0400

    Started stacks

---
+++ src/main/java/sandbox/stacks/StackMin.java
@@ -0,0 +1,31 @@
+package sandbox.stacks;
+
+import java.util.Stack;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class StackMin extends Stack<StackMin.NodeWithMin> {
+
+	private static final long serialVersionUID = 1L;
+
+	public void push(int value) {
+		int newMin = Math.min(value, min());
+		super.push(new NodeWithMin(value, newMin));
+	}
+
+	public int min() {
+		if (this.isEmpty()) {
+			return Integer.MAX_VALUE;
+		} else {
+			return peek().getMin();
+		}
+	}
+
+	@Data
+	@AllArgsConstructor
+	public static class NodeWithMin {
+		private int value;
+		private int min;
+	}
+}
---
+++ src/main/java/sandbox/stacks/StackMin2.java
@@ -0,0 +1,32 @@
+package sandbox.stacks;
+
+import java.util.Stack;
+
+public class StackMin2 extends Stack<Integer> {
+	
+	private static final long serialVersionUID = 1L;
+	private Stack<Integer> mins = new Stack<Integer>();
+	
+	public void push(int value) {
+		if (value <= min()) {
+			mins.push(value);
+		}
+		super.push(value);
+	}
+	
+	public Integer pop() {
+		int value = super.pop();
+		if (value == min()) {
+			mins.pop();
+		}
+		return value;
+	}
+	
+	public int min() {
+		if (mins.isEmpty()) {
+			return Integer.MAX_VALUE;
+		}
+		return mins.peek();
+	}
+
+}
