7fab0e0390da90d59eea58f1d66dc1f528ff5427
ng <nick.goupinets@gmail.com>
2019-08-27 16:16:49 -0400

    Added stats utils

---
+++ src/main/java/sandbox/stats/StatUtils.java
@@ -0,0 +1,28 @@
+package sandbox.stats;
+
+public class StatUtils {
+
+	public static double getArithmeticMean(double[] values) {
+		double sum = 0;
+		for (double d : values) {
+			sum += d;
+		}
+		return sum / values.length;
+	}
+
+	public static double getVariance(double[] values) {
+		double mean = getArithmeticMean(values);
+
+		double diffSum = 0;
+		for (double d : values) {
+			diffSum = Math.pow((d - mean), 2);
+		}
+
+		return diffSum / values.length;
+	}
+
+	public static double getStandardDeviation(double[] values) {
+		return Math.sqrt(getVariance(values));
+	}
+
+}
